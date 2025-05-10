9eff6d8bba5d6fef70ac36244b95d161bda8f39c
EduardoMP-Cloud <doredu@hotmail.com>
2025-05-10 11:40:40 -0500

    server.py

--- server.py
+++ server.py
@@ -69,4 +69,4 @@ def build_response(speech_text, video_url=None):
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
